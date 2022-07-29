57d3c569ebbdf636771e91f38c067617169eacaa
frederickpukay <28515879+FVPukay@users.noreply.github.com>
2022-07-28 22:24:20 -0700

    Adjust margin top navbar

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import './Logo.css';
 
 const Logo = () => {
     return (
-        <div className='ma4 mt0'>
+        <div className='ma4 mt4'>
             <Tilt className='Tilt br2 shadow-2' tiltMaxAngleX={30} tiltMaxAngleY={30} >
                 <div className='pa3'>
                     <img style={{paddingTop: '5px'}} alt='logo' src={academyLogo}/>
