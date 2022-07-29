228a9cd06bd25b955104b1568d5fcea1aa2e1c90
frederickpukay <28515879+FVPukay@users.noreply.github.com>
2022-07-28 23:35:47 -0700

    Change border color to primary

--- src/components/Scroll/Scroll.js
+++ src/components/Scroll/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '5px solid blue', height: '800px'}}>
+        <div style={{overflowY: 'scroll', border: '5px solid #0D6EFD', height: '800px'}}>
             {props.children}
         </div>
     );
